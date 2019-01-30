9545183cde89ba11d6ce744af91a172585c07cd4
govindsinghsolanki <govindsinghsolanki85@gmail.com>
2019-01-30 19:05:18 +0530

    HelloWorld File Commited

--- Example/src/login/HelloWorld.java
+++ Example/src/login/HelloWorld.java
@@ -3,7 +3,7 @@ package login;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i=1;i<20;i++)
+		for(int i=1;i<10;i++)
 		{
 			System.out.println("Hello World......");
 		}
